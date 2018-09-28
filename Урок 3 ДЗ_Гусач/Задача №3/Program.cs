c61c8d4346c8b8a2ada62e3255feeb094fda2127
rndig <gusach.misc@gmail.com>
2018-09-28 21:42:15 +0300

    28.09.2018 21_42

--- Урок 3 ДЗ_Гусач/Задача №3/Program.cs
+++ Урок 3 ДЗ_Гусач/Задача №3/Program.cs
@@ -37,6 +37,27 @@ namespace Задача__3
             this.denominator = denominator;
         }
 
+        public int Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+        public int Denominator
+        {
+            get { return denominator; }
+            set
+            {
+                if (value != 0)
+                {
+                    denominator = value;
+                }
+                else throw new Exception("Denominator sould not be equal to zero!");             
+
+            }
+        }
+
+        
+
         public Fraction Multiply(Fraction x2)
         {
             Fraction x3 = new Fraction();
@@ -84,19 +105,84 @@ namespace Задача__3
             }
             return x3;
         }
-        public Fraction Simplify(Fraction)
+        public Fraction Simplify(Fraction x)
         {
-            int i =
+            
+            int i=2,min;
+            if (x.numerator < x.denominator)
+            {
+                min = x.numerator;
+            }
+            else
+            {
+                min = x.denominator;
+            }
+            while(i<=min)
+            {
+                if(x.numerator % i == 0 && x.denominator % i == 0)
+                {
+                    x.numerator = x.numerator / i;
+                    x.denominator = x.denominator / i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return x;
         }
+        public string Show()
+        {
+            string print = numerator.ToString() + "/" + denominator.ToString();
+            return print;
+        }
+
 
     }
     class Program
     {
+
+        static void Define(Fraction Input)
+        {
+            Console.Write("Enter numerator  of fraction :");
+            Int32.TryParse(Console.ReadLine(), out int n);
+            Input.Numerator = n;
+            Console.Write("Enter denominator of fraction :");
+            Int32.TryParse(Console.ReadLine(), out int d);
+            Input.Denominator = d;
+        }
         static void Main()
         {
-            
+            Fraction L; 
+            L = new Fraction(1,1);
+            Console.WriteLine("Fraction L");
+            Define(L);
+
+            Fraction M;
+            M = new Fraction(1,1);
+            Console.WriteLine("Fraction M");
+            Define(M);
+
+            Fraction P;
+            P = new Fraction(1, 1);
+
+            Console.WriteLine("Fraction L :" +  L.Show() + "  Fraction M :" + M.Show());
 
 
+            P = M.Plus(L);
+            Console.WriteLine("L + M =" + P.Show());
+
+            P = M.Divide(L);
+            Console.WriteLine("L / M =" + P.Show());
+
+            P = M.Multiply(L);
+            Console.WriteLine("L * M =" + P.Show());
+
+            P = P.Simplify(P);
+            Console.WriteLine("Simplified L * M =" + P.Show());
+
+            Console.ReadKey();
+            Console.WriteLine("Press any key for exit ");
 
 
         }
