726e5cde76f06e8aeef1fb639f6eb72bbabe35f9
rndig <gusach.misc@gmail.com>
2018-09-28 16:12:24 +0300

    _

--- Урок 3 ДЗ_Гусач/Задача №1/Program.cs
+++ Урок 3 ДЗ_Гусач/Задача №1/Program.cs
@@ -3,13 +3,130 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// 
+/// Игорь Гусач 
+/// 
+/// 
+/// Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса. 
+/// 
+/// 
+/// </summary>
 namespace Задача__1
 {
+    class Complex
+    {
+        double re;
+        double im;
+        
+
+        public Complex()
+        {
+            re = 0;
+            im = 0;
+        }
+        public Complex(double re,double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+
+        public Complex Plus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = x2.im + im;
+            x3.re = x2.re + re;
+            return x3;
+        }
+
+        public Complex Minus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = im - x2.im;
+            x3.re = re - x2.re;
+            return x3;
+        }
+
+        public Complex Multiply(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = im*x2.re + re*x2.im;
+            x3.re = re*x2.re + im*x2.im;
+            return x3;
+        }
+        public Complex Divide(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = (im*x2.re - re*x2.im) / (x2.re*x2.re + x2.im*x2.im);
+            x3.re = (re*x2.re + im*x2.im) / (x2.re*x2.re + x2.im*x2.im);
+            return x3;
+        }
+        public double Re
+        {
+            get { return re; }
+            set { re = value; }
+        }
+        public double Im
+        {
+            get { return im; }
+            set
+            {
+                if (value >= 0) im = value;
+                else im = -1 * value;
+                
+            }
+        }
+        public  string toString()
+        {            
+            return re.ToString("F2") + " + " + im.ToString("F2") + "i";
+            
+        }
+    }
     class Program
     {
+        static void Define(Complex Input)
+        {            
+            Console.Write("Enter real part of complex number :");
+            Double.TryParse(Console.ReadLine(), out double nRe);
+            Input.Re = nRe;
+            Console.Write("Enter imaginary part of complex number :");
+            Double.TryParse(Console.ReadLine(), out double nIm);
+            Input.Im = nIm;            
+        }
+
         static void Main(string[] args)
         {
+
+
+
+            Complex P;
+            P = new Complex(1,1);
+            
+            Complex Q;
+            Q = new Complex(1, 1);
+
+            Complex W;
+            W = new Complex(1, 1);
+
+            Console.WriteLine("Define your complex number P:\n");
+            Define(P);
+
+            Console.WriteLine("\n\nDefine your complex number Q:\n");
+            Define(Q);
+
+            Console.WriteLine("\nP = " + P.toString());
+            Console.WriteLine("\nQ = " + Q.toString());
+
+            W = P.Plus(Q);
+            Console.WriteLine("\n\n\nP + Q = " + W.toString());
+            W = P.Minus(Q);
+            Console.WriteLine("P - Q = " + W.toString());
+            W = P.Divide(Q);
+            Console.WriteLine("P / Q = " + W.toString());
+            W = P.Multiply(Q);
+            Console.WriteLine("P * Q = " + W.toString());
+            Console.ReadKey();
+            Console.WriteLine("Press any key for exit ");
         }
     }
 }
---
+++ Урок 3 ДЗ_Гусач/Задача №3/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 
+/// Игорь Гусач
+/// 
+/// Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел. 
+/// 
+/// Предусмотреть методы сложения, вычитания, умножения и деления дробей. 
+/// 
+/// Написать программу, демонстрирующую все разработанные элементы класса.
+/// 
+/// ** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать 
+/// исключение ArgumentException("Знаменатель не может быть равен 0");
+/// 
+/// Добавить упрощение дробей.
+/// </summary>
+namespace Задача__3
+{
+    class Fraction
+    {
+        int numerator;
+        int denominator;
+
+        public Fraction()
+        {
+            numerator = 1;
+            denominator = 1;
+        }
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public Fraction Multiply(Fraction x2)
+        {
+            Fraction x3 = new Fraction();
+            x3.numerator = numerator * x2.numerator;
+            x3.denominator = denominator * x2.denominator;
+            return x3;
+        }
+
+        public Fraction Divide(Fraction x2)
+        {
+            Fraction x3 = new Fraction();
+            x3.numerator = numerator * x2.denominator;
+            x3.denominator = denominator * x2.numerator;
+            return x3;
+        }
+
+        public Fraction Plus(Fraction x2)
+        {
+            Fraction x3 = new Fraction();
+            if (x2.denominator == denominator)
+            {
+                x3.numerator = numerator + x2.numerator;
+                x3.denominator = denominator;
+            }
+            else
+            {
+                x3.numerator = numerator * x2.denominator + denominator * x2.numerator;
+                x3.denominator = denominator * x2.denominator;
+            }
+            return x3;
+        }
+
+        public Fraction Minus(Fraction x2)
+        {
+            Fraction x3 = new Fraction();
+            if (x2.denominator == denominator)
+            {
+                x3.numerator = numerator - x2.numerator;
+                x3.denominator = denominator;
+            }
+            else
+            {
+                x3.numerator = numerator * x2.denominator - denominator * x2.numerator;
+                x3.denominator = denominator * x2.denominator;
+            }
+            return x3;
+        }
+        public Fraction Simplify(Fraction)
+        {
+            int i =
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            
+
+
+
+
+        }
+    }
+}
