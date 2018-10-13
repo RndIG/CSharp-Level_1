2b3f06d5cfa76611226ba547665f30223f4b13d7
rndig <gusach.misc@gmail.com>
2018-10-13 15:08:06 +0300

    13.10.2018

---
+++ Урок 7 ДЗ_Гусач/Задача №1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Задача__1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnCommand3 = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.clicks = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.Label();
+            this.clicksHeader = new System.Windows.Forms.Label();
+            this.btnCommand4 = new System.Windows.Forms.Button();
+            this.btnCommand5 = new System.Windows.Forms.Button();
+            this.aimNumber = new System.Windows.Forms.Label();
+            this.aimHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.Location = new System.Drawing.Point(615, 68);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand1.TabIndex = 0;
+            this.btnCommand1.Text = "+1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.btnCommand1_Click);
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.Location = new System.Drawing.Point(615, 133);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "x2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.btnCommand2_Click);
+            // 
+            // btnCommand3
+            // 
+            this.btnCommand3.Location = new System.Drawing.Point(615, 197);
+            this.btnCommand3.Name = "btnCommand3";
+            this.btnCommand3.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand3.TabIndex = 2;
+            this.btnCommand3.Text = "reset";
+            this.btnCommand3.UseVisualStyleBackColor = true;
+            this.btnCommand3.Click += new System.EventHandler(this.btnCommand3_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(125, 77);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(13, 13);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "0";
+            // 
+            // clicks
+            // 
+            this.clicks.AutoSize = true;
+            this.clicks.Location = new System.Drawing.Point(125, 338);
+            this.clicks.Name = "clicks";
+            this.clicks.Size = new System.Drawing.Size(13, 13);
+            this.clicks.TabIndex = 4;
+            this.clicks.Text = "0";
+            // 
+            // number
+            // 
+            this.number.AutoSize = true;
+            this.number.Location = new System.Drawing.Point(56, 78);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(44, 13);
+            this.number.TabIndex = 5;
+            this.number.Text = "Number";
+            // 
+            // clicksHeader
+            // 
+            this.clicksHeader.AutoSize = true;
+            this.clicksHeader.Location = new System.Drawing.Point(59, 337);
+            this.clicksHeader.Name = "clicksHeader";
+            this.clicksHeader.Size = new System.Drawing.Size(35, 13);
+            this.clicksHeader.TabIndex = 6;
+            this.clicksHeader.Text = "Clicks";
+            // 
+            // btnCommand4
+            // 
+            this.btnCommand4.Location = new System.Drawing.Point(615, 255);
+            this.btnCommand4.Name = "btnCommand4";
+            this.btnCommand4.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand4.TabIndex = 7;
+            this.btnCommand4.Text = "Undo";
+            this.btnCommand4.UseVisualStyleBackColor = true;
+            this.btnCommand4.Click += new System.EventHandler(this.btnCommand4_Click);
+            // 
+            // btnCommand5
+            // 
+            this.btnCommand5.Location = new System.Drawing.Point(615, 338);
+            this.btnCommand5.Name = "btnCommand5";
+            this.btnCommand5.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand5.TabIndex = 8;
+            this.btnCommand5.Text = "New Game";
+            this.btnCommand5.UseVisualStyleBackColor = true;
+            this.btnCommand5.Click += new System.EventHandler(this.btnCommand5_Click);
+            // 
+            // aimNumber
+            // 
+            this.aimNumber.AutoSize = true;
+            this.aimNumber.Location = new System.Drawing.Point(477, 348);
+            this.aimNumber.Name = "aimNumber";
+            this.aimNumber.Size = new System.Drawing.Size(0, 13);
+            this.aimNumber.TabIndex = 9;
+            // 
+            // aimHeader
+            // 
+            this.aimHeader.AutoSize = true;
+            this.aimHeader.Location = new System.Drawing.Point(400, 348);
+            this.aimHeader.Name = "aimHeader";
+            this.aimHeader.Size = new System.Drawing.Size(36, 13);
+            this.aimHeader.TabIndex = 10;
+            this.aimHeader.Text = "Aim at";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.aimHeader);
+            this.Controls.Add(this.aimNumber);
+            this.Controls.Add(this.btnCommand5);
+            this.Controls.Add(this.btnCommand4);
+            this.Controls.Add(this.clicksHeader);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.clicks);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnCommand3);
+            this.Controls.Add(this.btnCommand2);
+            this.Controls.Add(this.btnCommand1);
+            this.Name = "Form1";
+            this.Text = "Doubler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnCommand3;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label clicks;
+        private System.Windows.Forms.Label number;
+        private System.Windows.Forms.Label clicksHeader;
+        private System.Windows.Forms.Button btnCommand4;
+        private System.Windows.Forms.Button btnCommand5;
+        private System.Windows.Forms.Label aimNumber;
+        private System.Windows.Forms.Label aimHeader;
+    }
+}
+
---
+++ Урок 7 ДЗ_Гусач/Задача №22/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// 
+/// Гусач Урок 7 ДЗ задача № 2
+/// 
+/// Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от
+/// больше или меньше загаданное число введенного. Для ввода данных от человека
+/// используется элемент TextBox.
+/// 
+/// </summary>
+namespace Задача__22
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        string text;
+        int answer;
+        int answerP;
+        int tries = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Random r= new Random();
+            n = r.Next(1, 100);
+            textBoxAnswer.ReadOnly = false;
+            labelTries.Text = "0";
+            tries = 0;
+            labelResult.Text = "";
+        }
+
+        private void textBoxAnswer_TextChanged(object sender, EventArgs e)
+        {            
+            
+        }
+
+        private void enterAnswer_Click(object sender, EventArgs e)
+        {
+            
+            tries++;
+            labelTries.Text = tries.ToString();
+            bool status = int.TryParse(textBoxAnswer.Text, out answerP);
+            answer = answerP;
+            if (answer < 1 || answer > 100)
+            {
+                labelResult.Text = "Number is not in range";
+            }
+            if (!status)
+            {
+                labelResult.Text = "That is not a valid number,or not a number at all!";
+            }
+            if (answer == n)
+            {
+                labelResult.Text = "Right!";
+            }           
+            if (answer > n && answer <= 100)
+            {
+                labelResult.Text = "Number is too big";
+            }
+            else if(answer < n && answer >0)
+            {
+                labelResult.Text = "Number is too low";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
